6d040b99ef45ab5d5b19d8e9e21eec525dba13bf
Alexandre Passant <alex@passant.org>
2014-05-21 23:11:39 +0100

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='echoplot',
-    version='0.1.0',
+    version='0.1.1',
     author='Alexandre Passant',
     author_email='alex@passant.org',
     packages=['echoplot'],
